2bb95a2ec9fed1b5d5769ac21d7374fb0494840e
Tchoupinax <corentinfiloche@hotmail.fr>
2022-01-15 08:27:44 +0100

    feat: add REGION variable

--- script.sh
+++ script.sh
@@ -1,8 +1,16 @@
 # stop if vercel token is missing
-[ -z $TOKEN ]&&printf "Token is missing"&&exit 1
+[ -z $TOKEN ] && printf "Token is missing" && exit 1
 
 # set --prod flag only on production deployments
-if [ -z $IS_PROD ];then printf "\nTEST DEPLOYMENT\n";else printf "\nPRODUCTION DEPLOYMENT\n"&&PROD="--prod";fi
+if [ -z $IS_PROD ];then
+  printf "\nTEST DEPLOYMENT\n";
+else
+  printf "\nPRODUCTION DEPLOYMENT\n"&&PROD="--prod";
+fi
 
 # run deployment and return url
-URL=$(vercel --confirm --token $TOKEN $PROD)&&printf "\nDEPLOYMENT SUCCESSFUL\n$URL"
+if [ -z $REGION ];then
+  URL=$(vercel --confirm --token $TOKEN $PROD) && printf "\nDEPLOYMENT SUCCESSFUL\n$URL"
+else
+  URL=$(vercel --confirm --regions $REGION --token $TOKEN $PROD) && printf "\nDEPLOYMENT SUCCESSFUL\n$URL"
+fi
